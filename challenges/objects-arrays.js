d90a2b189cc21f0b20927af1512940cf37c698a4
Owlspec3086 <ethical.worker3@gmail.com>
2020-03-27 15:57:46 -0400

    jScript sprint

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -144,7 +144,7 @@ console.log(lowPopulationAnimals);
 /* Request 4: .reduce() 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 */
-const populationTotal = zooAnimals.reduce((total, animal) => total + animal.population);
+const populationTotal = zooAnimals.reduce((total, animal) => {return total + animal.population}, 0);
 
 console.log(populationTotal);
 
